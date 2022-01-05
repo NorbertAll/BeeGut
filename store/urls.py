e80f0f1129b683dd9d929e51ad123e1c59ca79a7
NorbertAll <norbert_gutkowski@interia.pl>
2022-01-05 22:44:19 +0100

    store with beekeeping products

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('store/', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('', views.aboutus, name="aboutus"),
+    path('contact/', views.contact, name="contact"),
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+]
